18663aa6557139e1368d951f4f16554fcb99026a
Scott Hutley <scotthutley1@comcast.net>
2021-06-11 16:22:22 -0700

    Add test script in js (all stricken right now)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 // I am no closer to figuring this out than I was 3 hours ago. I've spent the entire morning researching old lessons to see if ANYTHING will trigger or otherwise clue me into where I start, but as of right now it's just not happening for me. 
 
 // Went to lunch at noon, came back at 1 and continued going through old lessons. It's 2pm now. I still haven't found what I'm looking for. Trying not to let it get to me and now officially dealing with a migraine as well. I've gone ALL the way back now to last week's lessons as I suspect what I'm truly missing is the full understanding of everything I was trying to make up on lost time for back then. It's good refresher information, but as yet it's not helping me. I don't even know why I'm writing this. It feels good to journal my struggle though so that's what I'm doing. Obviously this will all go away once I've figured out the delima I'm in. 
+
+// It's almost 4 now, but I might at least have a direction to go in.
+
+// const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// array.toString()
+
+// const number = 0
+// const convertedNumber = number.toString();
+// result is that 0 === "0"
